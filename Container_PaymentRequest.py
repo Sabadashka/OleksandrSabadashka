cfea33087c7ca21fcff6bb980e4fca38d7a99420
Sabadashka <90348773+Sabadashka@users.noreply.github.com>
2021-11-24 00:40:27 +0200

    Програмування 3,4

---
+++ Container_PaymentRequest.py
@@ -0,0 +1,151 @@
+from Validation import Validation
+from Payment_Request import PAYMENT_REQUEST
+from Payment_Request import ExceptionPayment
+
+
+class ContainerPAYMENT_REQUEST: 
+    def __init__(self, file_name = "None"):
+        self.__array_of_elements = []
+        self.__file_name = file_name
+        self.__validation = Validation()
+
+
+    def __str__(self):
+        string = ""
+        for payment in self.__array_of_elements:
+            string += str(payment) + "\n" + "-"*30 + "\n"
+        return string
+
+
+    def __repr__(self):
+        return self.__str__()
+
+
+    def read_from_file(self, value = "None"):
+        if (self.__validation.validate_file("File name", value)) is True: 
+            file = open(value)
+
+        
+        self.__array_of_elements = []
+        number = success = 0
+
+        array_values = [] 
+        for i in file:
+            if i[0] == "-": continue
+            i = i.split()
+            array_values.append(i[1])
+            
+        array_values.reverse()
+
+        while len(array_values) > 0:
+            try:
+                number += 1
+                array_attr = []
+                for i in range(len(PAYMENT_REQUEST.array_of_names)):
+                    array_attr.append(array_values.pop())
+
+                payment = PAYMENT_REQUEST(*array_attr)
+
+            except ValueError as message:
+                print(message)
+                print("Номер " + str(number) + " є дефектним")
+                while len(array_values) > 0 and len(array_values) % len(PAYMENT_REQUEST.array_of_names) != 0:
+                    array_values.pop()
+                continue
+            self.__array_of_elements.append(payment)
+            success += 1
+        print("\n" + str(success) + "\033[94m об'єкти були успішно прочитані з файлу: \033[0m" + str(value))
+
+
+    def write_to_file(self, value):
+        if self.__validation.validate_file("File name", value) is True:
+            file = open(value, mode="w")
+
+        for payments in self.__array_of_elements:
+            file.write(str(payments))
+            file.write("\n-----------------------\n")
+        file.close()
+
+
+    def append(self, payment):    
+        self.__validation.validate_PAYMENT("payment", payment)
+        
+        self.__array_of_elements.append(payment)
+        if self.__file_name != "None":
+            self.write_to_file(self.__file_name)
+        print("Об’єкт успішно додано")
+
+
+    def delete(self, ID):
+        self.__validation.validate_ID(lambda id : id)
+        index = 0
+        for payment in self.__array_of_elements:
+            if str(payment.get_id()) == str(ID):
+                self.__array_of_elements.pop(index)
+                if self.__file_name != "None":
+                    self.write_to_file(self.__file_name)
+                print("Об’єкт успішно видалено")
+                return
+            index += 1
+        else: 
+            raise ExceptionPayment("Об’єкт не знайдено або контейнер порожній")
+
+
+    def search_in_container(self, search_string):
+        wanted_payments = ContainerPAYMENT_REQUEST()
+        counter_of_found_elements = 0
+
+        for payment in self.__array_of_elements:
+            for getter in payment.getAll_getters():
+                if str(getattr(payment, getter)()).find(str(search_string)) != -1:
+                    wanted_payments.append(payment)
+                    counter_of_found_elements += 1
+                    break
+
+        print("Платежі успішно знайдено: " + str(counter_of_found_elements))
+        return wanted_payments
+
+
+    def sort(self, key):
+        dictionaryGets = {"1" : "get_id", "2" : "get_amount", "3" : "get_currency", "4" : "get_payer_email", 
+                          "5" : "get_payment_request_date", "6" : "get_payment_due_to_date", "7" : "get_transaction_id"}
+        try:
+            if type(getattr(self.__array_of_elements[0], dictionaryGets[key])) is str:
+                self.__array_of_elements.sort(key = lambda payment : getattr(payment, dictionaryGets[key])().lower())
+            else:
+                self.__array_of_elements.sort(key = lambda payment : getattr(payment, dictionaryGets[key])())
+
+            if self.__file_name != "None":
+                self.write_to_file(self.__file_name)
+
+        except:
+            print("Контейнер не можна сортувати")
+            raise ExceptionPayment("Ключ " + key + " відсутній")
+
+        print("Контейнер було успішно відсортовано за атрибутом: " + key)
+
+
+    def edit(self, ID, key, value):  
+        for payment in self.__array_of_elements:
+            if str(payment.get_id()) == str(ID):
+                dictionarySets = {"1" : payment.set_id, "2" : payment.set_amount, "3" : payment.set_currency, "4" : payment.set_payer_email, 
+                                  "5" : payment.set_payment_request_date, "6" : payment.set_payment_due_to_date, "7" : payment.set_transaction_id}
+                try:
+                    dictionarySets[key](value)
+                except KeyError:
+                    raise ExceptionPayment("Ключ " + key + " втрачено")
+
+                if self.__file_name != "None":
+                        self.write_to_file(self.__file_name)
+                print("Атрибут успішно відредаговано")
+                return
+        raise ExceptionPayment("Елемент з ідентифікатором " + str(ID) + " відсутній")
+
+
+    def set_the_file(self, filename):
+        self.__file_name = filename
+
+
+    def __len__(self):
+        return len(self.__array_of_elements)
+
